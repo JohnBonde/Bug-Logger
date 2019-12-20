705a48dc1530cd540f1e4c47535b1f94f0cf6a14
John Bonde <jjbonde1@gmail.com>
2019-12-20 15:57:01 -0700

    first front end commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="container-fluid">
+      <div class="row">
+        <div class="col-6" id="home-link">
+          <router-link to="/">
+            <h3>Bug Logger</h3>
+          </router-link>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -19,6 +24,7 @@
 
 #nav {
   padding: 30px;
+  background-color: cornflowerblue;
 }
 
 #nav a {
@@ -27,6 +33,10 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+}
+#home-link {
+  display: flex;
+  justify-content: flex-start;
 }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <header class="row">
+      <div class="col" id="report">
+        <button type="button" class="btn btn-outline-dark">Report</button>
+      </div>
+    </header>
+    <main class="row"></main>
+    <Bugs />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Bugs from "@/components/Bugs.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    Bugs
   }
-}
+};
 </script>
+<style>
+#report {
+  display: flex;
+  justify-content: flex-end;
+}
+button {
+  margin-top: -4.5em;
+  height: 40px;
+  width: 100px;
+}
+</style>
